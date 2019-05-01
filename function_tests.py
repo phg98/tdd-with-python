a3017ce9668fa0abc406598231cf13b636ae1843
Harry Park <hg98.park@hanwha.com>
2019-05-01 22:43:04 +0900

    Add home.html and change test

--- function_tests.py
+++ function_tests.py
@@ -1,5 +1,8 @@
+import time
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,6 +15,23 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('일정관리', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('일정목록', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '할일을 입력하세요')
+
+        inputbox.send_keys('시장에서 미역 사기')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: 시장에서 미역 사기' for row in rows))
+
+        self.fail('테스트 종료')
 
 
 if __name__ == '__main__':
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<!Doctype html><html><title>일정관리</title></html>\n')
+    return render(request, 'home.html')
