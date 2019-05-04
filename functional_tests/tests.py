bc3c62d9f12a8a634829d85593a96f28bbdfc7ab
Harry Park <hg98.park@hanwha.com>
2019-05-04 13:13:28 +0900

    Add test for seperate user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,12 +28,12 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), '할일을 입력하세요')
 
-        # input 1 item
+        # Enter input redirects to new URL and add item to list
         inputbox.send_keys('시장에서 미역 사기')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(5)
-
-        # check 1 item
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 시장에서 미역 사기')
 
         # input 1 more item
@@ -45,6 +45,34 @@ class NewVisitorTest(LiveServerTestCase):
         # two items in the list
         self.check_for_row_in_list_table('1: 시장에서 미역 사기')
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+
+        # new user francis connects
+
+        ## use new browser sesstoin
+        self.browser.quit()
+        self.browser = webdriver.Firefox();
+
+        # Francis connects to home page.
+        # Edith's list should NOT be shown
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('미역', page_text)
+        self.assertNotIn('공작깃털', page_text)
+
+        # Francis enter new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets new URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Check edith's item is NOT on the list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털', page_text)
+        self.assertIn('우유', page_text)
 
         self.fail('테스트 종료')
 
