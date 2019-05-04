9956e81759cd58033131920eaf7e60a6bae5353e
Harry Park <hg98.park@hanwha.com>
2019-05-04 12:51:54 +0900

    Change function test to DjangoTest

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
 import time
 import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -47,6 +48,3 @@ class NewVisitorTest(unittest.TestCase):
 
         self.fail('테스트 종료')
 
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
