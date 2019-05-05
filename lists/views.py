410f5b0ce5a71ac5430b0903b6b30419fe8662e0
Harry Park <hg98.park@hanwha.com>
2019-05-05 20:17:07 +0900

    Make url for each list

--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,13 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'item_list': items})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=(request.POST.get('item_text', '')), list=list_)
-    return redirect('/lists/only-list/')
+    return redirect('/lists/%d/' % list_.id)
